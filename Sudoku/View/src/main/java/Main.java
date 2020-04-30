2d781e100433e4149aa63b846524164ff5a26bf8
jakub_pietras <jacubeus@gmail.com>
2020-04-30 04:26:03 +0200

    Graphical interface.

--- Sudoku/View/src/main/java/Main.java
+++ Sudoku/View/src/main/java/Main.java
@@ -4,13 +4,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(
+                Objects.requireNonNull(
+                        getClass().getClassLoader().getResource("main_menu.fxml")
+                )
+        );
+        primaryStage.setTitle("Sudoku");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.getScene().getStylesheets().add("main_menu_style.css");
         primaryStage.show();
     }
 

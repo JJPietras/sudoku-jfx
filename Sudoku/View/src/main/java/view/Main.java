bb50db21e221976970ec826acd7d1c27e60791de
bartlomiej_graczyk <bgraczyk.mail@gmail.com>
2020-05-13 08:45:11 +0200

    App internationalization process started.

--- Sudoku/View/src/main/java/view/Main.java
+++ Sudoku/View/src/main/java/view/Main.java
@@ -1,36 +1,35 @@
 package view;
 
+import java.util.Objects;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.Objects;
-
 public class Main extends Application {
 
     static Stage stage;
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) {
         try {
-        stage = primaryStage;
-        Parent root = FXMLLoader.load(
-                Objects.requireNonNull(
-                        getClass().getClassLoader().getResource("main_menu.fxml")
-                )
-        );
-        primaryStage.setTitle("Sudoku");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.getScene().getStylesheets().add("main_menu_style.css");
-        primaryStage.show(); }
-        catch (Exception e) {
+            stage = primaryStage;
+            Parent root = FXMLLoader.load(
+                    Objects.requireNonNull(
+                            getClass().getClassLoader().getResource("main_menu.fxml")
+                    )
+            );
+            primaryStage.setTitle("Sudoku");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.getScene().getStylesheets().add("main_menu_style.css");
+            primaryStage.show();
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
